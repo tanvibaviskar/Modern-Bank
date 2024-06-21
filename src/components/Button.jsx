99fce5203704a2b39e0b6000643d120b8262de53
tanvibaviskar <145899846+tanvibaviskar@users.noreply.github.com>
2024-06-21 15:39:47 +0530

    Add files via upload

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Button = ({ styles }) => {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium outline-none ${styles} rounded-[10px]`}
+    >
+      Get Started
+    </button>
+  );
+};
+
+export default Button;
